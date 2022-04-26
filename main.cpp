f953232f4f699d18d1cec1565087cb382bde77ad
DragonJoker <dragonjoker59@hotmail.com>
2022-04-26 20:46:22 +0200

    Added files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vulkan/vulkan.h>
+#include <glfw/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+int main(){
+
+
+glfwInit();
+
+}
